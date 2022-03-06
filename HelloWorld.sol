78ab6bc88b5f99196cd7ec68a73538ed3a434492
gourav <gvirus21yt@gmail.com>
2022-03-06 13:28:39 +0530

    added HelloWorld contract

---
+++ HelloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    function get() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
